3e11e823f0b54d648fe597473b2759c08c1b2a9d
RowanDurrant <58739837+RowanDurrant@users.noreply.github.com>
2023-02-10 11:57:24 +0000

    figure updates

--- code/SNP_probs_1_gen.R
+++ code/SNP_probs_1_gen.R
@@ -1,6 +1,7 @@
 library(ggplot2)
+library(patchwork)
 
-sample(1:1000, 1) #216
+#sample(1:1000, 1) #216
 set.seed(216)
 
 a = rpois(100000, 0.1690)
@@ -17,7 +18,7 @@ p1 = ggplot(data=g, aes(x=a, y=Freq, fill = Method)) +
   geom_bar(position = "dodge", stat = 'identity') +
   theme_classic() +
   xlab("No. SNPs") + ylab("1 Generation")+
-  coord_cartesian(xlim = c(1, 9)) + ylim(0,1)+
+  coord_cartesian(xlim = c(1, 10)) + ylim(0,1)+
   theme(axis.title.x=element_blank(),
         axis.text.x=element_blank(),
         axis.ticks.x=element_blank(),
@@ -25,69 +26,6 @@ p1 = ggplot(data=g, aes(x=a, y=Freq, fill = Method)) +
   scale_fill_manual("", values = c("blue", "red"))
 
  
-a = rpois(100000, 0.1690*2)
-b = table(a)/sum(table(a))
-c = as.data.frame(b)
-c$Method = "Novel"
-d = rpois(100000, 0.1075*2)
-e = table(d)/sum(table(d))
-f = as.data.frame(e)
-f$Method = "Clock"
-colnames(f) = c("a", "Freq", "Method")
-g = rbind(c,f)
-p2 = ggplot(data=g, aes(x=a, y=Freq, fill = Method)) + 
-  geom_bar(position = "dodge", stat = 'identity') +
-  theme_classic() +
-  xlab("No. SNPs") + ylab("2 Generations")+
-  coord_cartesian(xlim = c(1, 9)) + ylim(0,1)+
-  theme(axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank(),
-        legend.position = "none")+
-  scale_fill_manual("", values = c("blue", "red"))
-
-a = rpois(100000, 0.1690*3)
-b = table(a)/sum(table(a))
-c = as.data.frame(b)
-c$Method = "Novel"
-d = rpois(100000, 0.1075*3)
-e = table(d)/sum(table(d))
-f = as.data.frame(e)
-f$Method = "Clock"
-colnames(f) = c("a", "Freq", "Method")
-g = rbind(c,f)
-p3 = ggplot(data=g, aes(x=a, y=Freq, fill = Method)) + 
-  geom_bar(position = "dodge", stat = 'identity') +
-  theme_classic() +
-  xlab("No. SNPs") + ylab("3 Generations") +
-  coord_cartesian(xlim = c(1, 9)) + ylim(0,1)+
-  theme(axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank(),
-        legend.position = "none")+
-  scale_fill_manual("", values = c("blue", "red"))
-
-a = rpois(100000, 0.1690*4)
-b = table(a)/sum(table(a))
-c = as.data.frame(b)
-c$Method = "Novel"
-d = rpois(100000, 0.1075*4)
-e = table(d)/sum(table(d))
-f = as.data.frame(e)
-f$Method = "Clock"
-colnames(f) = c("a", "Freq", "Method")
-g = rbind(c,f)
-p4 = ggplot(data=g, aes(x=a, y=Freq, fill = Method)) + 
-  geom_bar(position = "dodge", stat = 'identity') +
-  theme_classic() +
-  xlab("No. SNPs") + ylab("4 Generations")+
-  coord_cartesian(xlim = c(1, 9)) + ylim(0,1)+
-  theme(axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank(),
-        legend.position = "none")+
-  scale_fill_manual("", values = c("blue", "red"))
-
 a = rpois(100000, 0.1690*5)
 b = table(a)/sum(table(a))
 c = as.data.frame(b)
@@ -98,13 +36,33 @@ f = as.data.frame(e)
 f$Method = "Clock"
 colnames(f) = c("a", "Freq", "Method")
 g = rbind(c,f)
-p5 = ggplot(data=g, aes(x=a, y=Freq, fill = Method)) + 
+p2 = ggplot(data=g, aes(x=a, y=Freq, fill = Method)) + 
   geom_bar(position = "dodge", stat = 'identity') +
   theme_classic() +
   xlab("No. SNPs") + ylab("5 Generations")+
-  coord_cartesian(xlim = c(1, 9)) + ylim(0,1)+
-  theme(legend.position = "none")+
+  coord_cartesian(xlim = c(1, 10)) + ylim(0,1)+
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        legend.position = "none")+
   scale_fill_manual("", values = c("blue", "red"))
 
-library(patchwork)
-p1/p2/p3/p4/p5
+a = rpois(100000, 0.1690*10)
+b = table(a)/sum(table(a))
+c = as.data.frame(b)
+c$Method = "Novel Method"
+d = rpois(100000, 0.1075*10)
+e = table(d)/sum(table(d))
+f = as.data.frame(e)
+f$Method = "Clock Rate Method"
+colnames(f) = c("a", "Freq", "Method")
+g = rbind(c,f)
+p3 = ggplot(data=g, aes(x=a, y=Freq, fill = Method)) + 
+  geom_bar(position = "dodge", stat = 'identity') +
+  theme_classic() +
+  xlab("No. SNPs") + ylab("10 Generations") +
+  coord_cartesian(xlim = c(1, 10)) + ylim(0,1)+
+  theme(legend.position = "bottom")+
+  scale_fill_manual("", values = c("blue", "red"))
+
+p1/p2/p3
